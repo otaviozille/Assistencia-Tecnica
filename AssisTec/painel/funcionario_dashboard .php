4a950594261d2fa6849840132e8b575efffa1eae
laylasilva-ai <laylasilva1342@gmail.com>
2025-02-11 21:31:14 -0300

    Dashboard Funcionario

---
+++ AssisTec/painel/funcionario_dashboard .php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Funcionario</title>
+    <link rel="stylesheet" href="/AssisTec/style.css">
+</head>
+<body>
+    <h1>Dashboard Funcionario</h1>
+    <div class="Funcionario">
+        <div class="container">
+
+
+
+        
+        </div>
+    </div>
+</body>
+</html>
